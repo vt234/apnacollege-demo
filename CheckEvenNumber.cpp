d364992136e85b761f9cc203a34b11e278b89091
Vivek  Tiwari <vt8228345@gmail.com>
2025-03-17 22:44:52 +0530

    this code of check even number i n c

---
+++ CheckEvenNumber.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+    int a;
+    cout<<"enter a: ";
+    cin>>a;
+    cout<<a;
+
+    if  (a%2==0)
+        cout<<"the given number is even";
+    
+        else
+        cout<<"it is odd";
+    return 0;
+
+
+}
